b04f18c3b3db8ad6b3dacec6396618c2c781b9c8
yangfan-shine <yangfan-shine@qq.com>
2022-05-26 02:10:48 +0800

    增加分组颜色
    还剩的内容:
    1）heading active的功能
    2）post-container 和 markdown-body重复 冲突的问题
    3）清理无用样式

--- src/views/ArticleListView.vue
+++ src/views/ArticleListView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="post-list">
     <ul>
-      <li v-for="li_item in li_items" :key="li_item" :class="li_item.is_group ? 'group-item' : 'article-item'">
+      <li v-for="li_item in li_items" :key="li_item" :class="(li_item.is_group ? 'group-item group-color-'+li_item['color_index']%5 : 'article-item')"
+      >
         <router-link :class="li_item.is_group ? 'context-hover' : 'context-hover'"
           :id="li_item.is_group ? 'group_' + li_item.title : ''" :to="li_item.path">{{ li_item.title }}
         </router-link>
@@ -18,11 +19,10 @@ import { articlesGroupByTime, articlesGroupByTopic, articlesGroupByLabel } from 
 export default defineComponent({
   name: "ArticleListView",
   setup() {
-
     const router = useRouter()
     const route_path = router.currentRoute.value.path
     const li_items = ref()
-
+    
     let group_kind
     let group_map = new Map()
     if (route_path.startsWith('/archive')) {
@@ -42,13 +42,27 @@ export default defineComponent({
 
     let _li_items = [] as any
     if (group_map) {
+    let _color_index = 0
     for (let [group_key, group_value] of group_map.entries()) {
       console.log("entries2", group_key, group_key);
-      _li_items.push({ title: group_key, path: '/' + group_kind + '/#group_' + group_key, is_group: true })
+      _li_items.push({ title: group_key, path: '/' + group_kind + '/#group_' + group_key, is_group: true,color_index:_color_index })
       for (let i = 0; i < group_value.length; i++) {
         let article = group_value[i]
         _li_items.push({ title: article['title'].length > 0 ? article['title']+article['create_time']: 'No Name'+article['create_time'], path: '/article/' + article['name'], is_group: false })
       }
+
+      _color_index++
+    }
+
+        for (let [group_key, group_value] of group_map.entries()) {
+      console.log("entries2", group_key, group_key);
+      _li_items.push({ title: group_key, path: '/' + group_kind + '/#group_' + group_key, is_group: true,color_index:_color_index })
+      for (let i = 0; i < group_value.length; i++) {
+        let article = group_value[i]
+        _li_items.push({ title: article['title'].length > 0 ? article['title']+article['create_time']: 'No Name'+article['create_time'], path: '/article/' + article['name'], is_group: false })
+      }
+
+      _color_index++
     }
     li_items.value = _li_items
     }
@@ -84,4 +98,21 @@ export default defineComponent({
   line-height: 2.8em;
   border-left: 4px solid #eaeaea;
 }
+
+.group-color-0:before {
+  background: #1abc9c !important;
+}
+.group-color-1:before {
+  background: #3498db !important;
+}
+.group-color-2:before {
+  background: #9b59b6 !important;
+}
+.group-color-3:before {
+  background: #e67e22 !important;
+}
+.group-color-4:before {
+  background: #880000 !important;
+}
+
 </style>
